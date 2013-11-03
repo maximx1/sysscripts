68e1693fb6930f7a1e04d62e2488ebb5ad627902
Justin Walrath <walrathjaw@gmail.com>
2013-11-03 13:37:44 -0600

    Fixed some bugs. Running some live testing

--- game management/minecraft/java port listner/TCPListnerV2.java
+++ game management/minecraft/java port listner/TCPListnerV2.java
@@ -4,71 +4,63 @@ import java.io.*;
 
 public class TCPListnerV2
 {
+	private static final String[] minecraftProcGrep = new String[]{"bash", "-c", "screen -ls | grep minecraft"};
+	private static final String[] minecraftProcStart = new String[]{"bash", "-c", "/home/justin/Games/Minecraft/startMinecraft.sh"};
+	
 	public static void main(String args[]) throws Exception
-	{
-
-		private static final String[] minecraftProcGrep = new String[]{"bash", "-c", "screen -ls | grep minecraft"};
-		private static final String[] minecraftProcStart = new String[]{"bash", "-c", "/home/justin/Games/startMinecraft.sh"};
-		 
+	{	 
+		String clientSentence;
+		String capitalSentence;
+		ServerSocket welcomeSocket = new ServerSocket(25565);
+	
 		while(true)
 		{
-			String clientSentence;
-			String capitalSentence;
-			ServerSocket welcomeSocket = new ServerSocket(25565);
-		
-			while(true)
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			String input;
+			if(null != inFromClient.readLine())
 			{
-				Socket connectionSocket = welcomeSocket.accept();
-				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-				String input;
-				if(null != inFromClient.readLine())
-				{
-					break;
-				}
-			}
-
-			try
-			{
-				PrintWriter printWriter = new PrintWriter(new File("error.log"), true);
-				
-				Runtime runtime = Runtime.getRuntime();
-				Process process = runtime.exec(minecraftProcGrep);
-
-				BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-				String result = stdInput.readLine();
-				stdInput.close();
-				if(result != null && result.length() > 0)
-				{
-					printWriter.println("Port was called, minecraft is already up and running");
-				}
-				else
-				{
-					printWriter.println("Port was called, starting minecraft now");
-					process.waitFor();
-					runtime.exec(minecraftProcStart);
-					
-					//Check the process again
-					process = runtime.exec(minecraftProcGrep);
-
-					stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-					result = stdInput.readLine();
-					stdInput.close();
-					if(result != null || result.length() == 0)
-					{
-						printWriter.close();
-						continue;
-					}
-				}
-
-				printWriter.close();
 				break;
 			}
-			catch(IOException er)
+		}
+
+		try
+		{
+			welcomeSocket.close();
+			welcomeSocket = null;
+		}
+		catch(Exception er)
+		{
+			System.out.println(er.getMessage());
+		}
+
+		try
+		{
+			PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File("error.log"), true));
+			
+			Runtime runtime = Runtime.getRuntime();
+			Process process = runtime.exec(minecraftProcGrep);
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			String result = stdInput.readLine();
+			stdInput.close();
+			if(result != null && result.length() > 0)
 			{
-				//And eat
+				printWriter.println("Port was called, minecraft is already up and running");
 			}
+			else
+			{
+				printWriter.append("Port was called, starting minecraft now\n");
+				process.waitFor();
+				runtime.exec(minecraftProcStart);
+			}
+
+			printWriter.close();
+		}
+		catch(IOException er)
+		{
+			//And eat
 		}
 	}
 }
--- game management/minecraft/stopMinecraftCleanly.sh
+++ game management/minecraft/stopMinecraftCleanly.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 screen -p 0 -S minecraft -X eval 'stuff stop\015'
+nohup java -cp "/home/justin/Games/Minecraft/" TSPListnerV2 &
